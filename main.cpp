c94b298042b1f93f73f4eff1a5e7efc21ae77925
f4s4r <sesetimokhin@edu.hse.ru>
2024-01-20 14:00:02 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iterator>
+
+template <typename T>
+class Vector
+{
+    T* data_;
+    size_t count_;
+public:
+    class VectorIterator; // делаем так, чтобы не заморачиваться с friend'ом
+
+};
+
+
+
+
+int main() {
+    return 0;
+}
